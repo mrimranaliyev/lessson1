54d8e44f5fabf610c7790dfd032c4dacf5fffa0b
mrimranaliyev <imranalyev237@gmail.com>
2024-10-06 22:50:13 +0400

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ikirəqəmli ədəd daxil edin: ");
+        int a = scanner.nextInt();
+        int tens = a / 10;
+        int units = a % 10;
+        System.out.println(tens + " " + units);
     }
 }
